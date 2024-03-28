0e0612abf84f28b0ebf7cacd090705133db05183
MygJae <50861514+mygjae@users.noreply.github.com>
2024-03-28 11:29:14 +0900

    naver create account

--- src/main/java/com/example/security1/config/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/example/security1/config/oauth/PrincipalOauth2UserService.java
@@ -2,6 +2,9 @@ package com.example.security1.config.oauth;
 
 import com.example.security1.config.CustomBCryptPasswordEncoder;
 import com.example.security1.config.auth.PrincipalDetails;
+import com.example.security1.config.oauth.provider.GoogleUserInfo;
+import com.example.security1.config.oauth.provider.NaverUserInfo;
+import com.example.security1.config.oauth.provider.OAuth2UserInfo;
 import com.example.security1.domain.User;
 import com.example.security1.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +14,8 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
@@ -23,26 +28,44 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(userRequest); // google의 회원 프로필 조회
+
+        // code를 통해 구성한 정보
+        System.out.println("userRequest clientRegistration : " + userRequest.getClientRegistration());
+        // token을 통해 응답받은 회원정보
+        System.out.println("oAuth2User : " + oAuth2User);
+
+        return processOAuth2User(userRequest, oAuth2User);
+    }
+
+    public OAuth2User processOAuth2User(OAuth2UserRequest userRequest, OAuth2User oAuth2User) throws OAuth2AuthenticationException {
         System.out.println("getClientRegistration() = " + userRequest.getClientRegistration());
         System.out.println("getAccessToken() = " + userRequest.getAccessToken());
 
         // AccessToken요청 -> userRequest정보 -> loadUser함수 호출
 
-        OAuth2User oAuth2User = super.loadUser(userRequest);
-        System.out.println("getAttributes() = " + oAuth2User.getAttributes());
+        OAuth2UserInfo oAuth2UserInfo = null;
+        if (userRequest.getClientRegistration().getRegistrationId().equals("google")) {
+            System.out.println("구글 로그인 요청");
+            oAuth2UserInfo = new GoogleUserInfo(oAuth2User.getAttributes());
+        } else if (userRequest.getClientRegistration().getRegistrationId().equals("naver")) {
+            System.out.println("네이버 로그인 요청");
+            oAuth2UserInfo = new NaverUserInfo((Map) oAuth2User.getAttributes().get("response"));
+        } else {
+            System.out.println("지원 안함");
+        }
 
-        String provider = userRequest.getClientRegistration().getClientId();
-        String providerId = oAuth2User.getAttribute("sub");
+        String provider = oAuth2UserInfo.getProvider();
+        String providerId = oAuth2UserInfo.getProviderId();
         String username = provider + "-" + providerId;
         String pwd = bCryptPasswordEncoder.encode("겟인데어");
-        String email = oAuth2User.getAttribute("email");
+        String email = oAuth2UserInfo.getEmail();
         String role = "ROLE_USER";
 
         User userEntity = userRepository.findByUsername(username);
         if (userEntity == null) {
             userEntity = User.builder()
                     .username(username)
-                    .password(pwd)
                     .email(email)
                     .role(role)
                     .provider(provider)
