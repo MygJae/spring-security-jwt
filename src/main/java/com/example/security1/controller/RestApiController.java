5fb5e56fb95bbdeb1f674338fee4d72ad9c504d0
Mygjae <50861514+MygJae@users.noreply.github.com>
2024-04-05 23:48:49 +0900

    jwt 임시 토큰 테스트

--- src/main/java/com/example/security1/controller/RestApiController.java
+++ src/main/java/com/example/security1/controller/RestApiController.java
@@ -1,6 +1,7 @@
 package com.example.security1.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,6 +12,9 @@ public class RestApiController {
         return "<h1>home</h1>";
     }
 
-
+    @PostMapping("/token")
+    public String token() {
+        return "<h1>token</h1>";
+    }
 }
 
--- src/main/java/com/example/security1/filter/MyFilter3.java
+++ src/main/java/com/example/security1/filter/MyFilter3.java
@@ -1,12 +1,32 @@
 package com.example.security1.filter;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class MyFilter3 implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse res = (HttpServletResponse) servletResponse;
+        res.setCharacterEncoding("UTF-8");
+
+        //토큰: cos -> id,pw 정상적인 로그인 됬을때 만듬 -> header에 authorization의 토근값 가져오고 -> 검증만 하면 됨(RSA)
+        if (req.getMethod().equals("POST")) {
+            String headerAuth = req.getHeader("Authorization");
+            System.out.println(headerAuth);
+
+            if (headerAuth.equals("cos")) {
+                filterChain.doFilter(req, res);
+            } else {
+                PrintWriter out = res.getWriter();
+                out.println("인증 안됨");
+            }
+        }
+
         System.out.println("필터3");
-        filterChain.doFilter(servletRequest, servletResponse);
+        filterChain.doFilter(req, res);
     }
 }
